d6726376c0ff309af8a3a63f799e5bd3d83de585
minjjjae <csu05065@naver.com>
2020-07-28 15:47:34 +0900

    20200728_김민재

--- individual/minmin/statistics.py
+++ individual/minmin/statistics.py
@@ -1,14 +1,10 @@
 # 만든이 : 정우영
 # %%
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
-
-
-
-
 
 
 # %%
@@ -30,11 +26,13 @@ temp_df = pd.DataFrame(
         "price_mean": [],
         "like_mean": [],
         "score_mean": [],
+        "price_max": [],
+         "price_min": []
     }
 )
-subject = "c"
+subject = "java"
 for file_name in file_list:
-    df = pd.read_csv(f"../analyze_data/{subject}/{subject}_{file_name}.csv")
+    df = pd.read_csv(f"../../analyze_data/{subject}/{subject}_{file_name}.csv")
 
     if df["site"].count() == 0:
         continue
@@ -49,9 +47,9 @@ for file_name in file_list:
     temp_dict["lec_sum"] = lec_sum
 
     # 합계
-    # view_sum = df["view"].sum()
-    # view_sum
-    # temp_dict["view_sum"] = view_sum
+    view_sum = df["view"].sum()
+    view_sum
+    temp_dict["view_sum"] = view_sum
 
     # 평균
     view_mean = int(df["view"].mean())
@@ -70,6 +68,17 @@ for file_name in file_list:
         temp_dict["price_mean"] = price_mean
     else:
         temp_dict["price_mean"] = np.nan
+
+    price_df = df[df.price != 0]
+    # price 최댓값
+    price_max = price_df["price"].max()
+    temp_dict["price_max"] = price_max
+
+    # price 최솟값
+    price_min = price_df["price"].min()
+    temp_dict["price_min"] = price_min
+
+
 
     # like 평균
     if df["like"].isnull().sum() == 0:
@@ -92,25 +101,19 @@ for file_name in file_list:
 temp_df
 
 
-
-
-# 
-
 # %%
-#그래프 강의수
-site_view_sum=pd.DataFrame()
-site_view_sum["site"]=temp_df["site"]
-site_view_sum["view_sum"]=temp_df["view_sum"]
+# 그래프 강의수
+site_view_sum = pd.DataFrame()
+site_view_sum["site"] = temp_df["site"]
+site_view_sum["view_sum"] = temp_df["view_sum"]
 site_view_sum
-fig=plt.figure()
+fig = plt.figure()
 plt.bar(site_view_sum["site"], site_view_sum["view_sum"])
 plt.show()
 
 
-
 # %%
-#사이트별 강의수(pie그래프)
-import plotly.graph_objects as go
+# 사이트별 강의수(pie그래프)
 
 site_lec_sum = pd.DataFrame()
 site_lec_sum["site"] = temp_df["site"]
@@ -122,42 +125,48 @@ values = site_lec_sum["lec_sum"]
 
 fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent',
                              insidetextorientation='radial'
-                            )])
+                             )])
 
 fig.show()
 
 
-
 # %%
 # 사이트별 강의수와 조회수 비교
-plt.rcParams["font.family"]='NanumBarunGothic'
-plt.rcParams["font.size"]=10           
-plt.rcParams["figure.figsize"]=(10,4)
+plt.rcParams["font.family"] = 'NanumBarunGothic'
+plt.rcParams["font.size"] = 10
+plt.rcParams["figure.figsize"] = (10, 4)
 
-site_view_mean=pd.DataFrame()
+site_view_mean = pd.DataFrame()
 site_view_mean["site"] = temp_df["site"]
 site_view_mean["lec_sum"] = temp_df["lec_sum"]
-site_view_mean["view_mean"] = temp_df["view_mean"]
+site_view_mean["view_sum"] = temp_df["view_sum"]
 
 
 x = site_view_mean["site"]
-y1=site_view_mean["lec_sum"]
-y2=site_view_mean["view_mean"]
+y1 = site_view_mean["lec_sum"]
+y2 = site_view_mean["view_sum"]
 
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 
 
-line1=ax1.bar(x,y1,alpha=0.5)
-line2=ax2.plot(x,y2,'go--',color='green')
+line1 = ax1.bar(x, y1, alpha=0.5)
+line2 = ax2.plot(x, y2, 'go--', color='green')
 
-plt.legend((line1[0], line2[0]), ('강의수', '조회수'),loc='upper center')
-plt.grid(True,alpha=0.5)
+plt.legend((line1[0], line2[0]), ('강의수', '조회수'), loc='upper center')
+plt.grid(True, alpha=0.5)
 
 plt.show()
 
 # %%
 
+site_price_min = pd.DataFrame()
+site_price_min["site"] = temp_df["site"]
+site_price_min["price_min"] = temp_df["price_min"] 
+site_price_min
+fig = plt.figure()
+plt.bar(site_price_min["site"], site_price_min["price_min"])
+plt.show()
+
 
 # %%
-    
